40c03b2fb1d3e05d1143056858335c487e6582c0
igxjar <igorjaron.ij@gmail.com>
2026-08-23 14:06:36 +0200

    Initialize DataWarehouseProject database and schemas
    Drop existing DataWarehouse database if it exists and create a new DataWarehouseProject database with bronze, silver, and gold schemas.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,26 @@
 
+USE master;
+GO
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN 
+  ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+  DROP DATABASE DataWarehouse;
+END;
+GO
+  
+CREATE DATABASE DataWarehouseProject;
+GO
+
+USE DataWarehouse;
+GO
+
+  -- Create Schemas
+CREATE SCHEMA bronze;
+GO
+  
+CREATE SCHEMA silver;
+GO
+  
+CREATE SCHEMA gold;
+GO
